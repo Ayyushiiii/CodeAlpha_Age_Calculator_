e9580bef106ee50eb7c885081d9d442ed1ab24b1
Ayushi singh <98972995+Ayyushiiii@users.noreply.github.com>
2024-05-25 12:59:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+
+const ageCalculator = () =>{
+    const today = new Date();
+    const inputDate = new Date(document.getElementById("date-input").value);
+
+    const birthDetails = {
+        date : inputDate.getDate(),
+        month : inputDate.getMonth()+1 ,
+        year : inputDate.getFullYear(),
+    }
+
+    const currentYear = today.getFullYear();
+    const currentMonth = today.getMonth() +1;
+    const currentDate = today.getDate();
+
+    if(isFutureDate(birthDetails,currentDate,currentMonth,currentYear)){
+        alert("Not Born Yet");
+        displayResult("-", "-" ,"-");
+        return;
+    }
+   
+    const {years, months , days} = calculateAge(
+        birthDetails,
+         currentYear,
+         currentMonth,
+         currentDate,);
+
+         displayResult(days, months , years);
+    };
+         const isFutureDate = (birthDetails,currentDate,currentMonth,currentYear) =>{
+            return (
+                 birthDetails.year > currentYear ||
+                (birthDetails.year === currentYear &&
+                (birthDetails.month > currentMonth ||
+                    (birthDetails.month === currentMonth &&
+                        birthDetails.date > currentDate
+                    )))
+            );
+           
+         };
+
+const calculateAge = (birthDetails, currentYear,currentMonth,currentDate) =>{
+    let years = currentYear - birthDetails.year;
+    let months ;
+    let days ;
+
+    if(currentMonth < birthDetails.month){
+        years--;
+        months = 12 - (birthDetails.month - currentMonth)
+    }
+    else {
+        months = currentMonth - birthDetails.month
+    }
+
+    if(currentDate < birthDetails.date){
+        months-- ; 
+       const lastMonth = currentMonth=== 1 ? 12 :currentMonth-1;
+       const daysInlastMonth = getDaysInMonth(lastMonth, currentYear);
+       days = daysInlastMonth - (birthDetails.date - currentDate)
+    }
+    else {
+        days = currentDate - (birthDetails.date)
+    }
+    return {years , months , days};
+}
+
+const getDaysInMonth = (month , year) =>{
+    const isLeapYear = year%4 === 0 &&(year %100 != 0 || year% 4 ===0)
+
+    const getDaysInMonth = [
+        31,
+        isLeapYear? 29:28,
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31
+    ]
+    return getDaysInMonth[month -1]
+};
+
+const displayResult = (bDate , bMonth , bYear) =>{
+    document.getElementById("years").textContent = bYear;
+    document.getElementById("months").textContent = bMonth;
+    document.getElementById("days").textContent = bDate;
+}
+
+document.getElementById("calc-btn").addEventListener("click", ageCalculator);
